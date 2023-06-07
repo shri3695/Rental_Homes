8f4a6f9ca07504484401ad96dc8fc9471531072e
shri3695 <102024842+shri3695@users.noreply.github.com>
2023-06-07 19:40:30 +0530

    prior

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import "swiper/css";
+import "./Carousel.css";
+import data from "../../utils/slider.json";
+import { sliderSettings } from "../../utils/common";
+const Carousel = () => {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Top Rated Rentals</span>
+        </div>
+        <Swiper {...sliderSettings}>
+          <SliderButtons />
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r-card">
+                <img src={card.image} alt="resto" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>₹</span>
+                  <span>{card.price}</span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+export default Carousel;
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className="flexCenter r-buttons">
+      <button onClick={() => swiper.slidePrev()}>&lt;</button>
+      <button onClick={() => swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
